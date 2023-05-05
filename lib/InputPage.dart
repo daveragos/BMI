ca5977c5607553810dbf7b9112cffd69dff8dd6c
Dave RaGoose <daveyeinde@gmail.com>
2023-05-05 03:01:02 +0300

    two widget class created in separeted file and enum gender

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -1,9 +1,15 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tester/IconWidget';
-import 'package:tester/customWidget';
+import 'package:tester/CardWidget';
+
+const activeColor = Color(0XFF1D1E33);
+const inactiveColor = Color(0XFF111328);
+const containerColor = Color(0XFFEB1555);
+const hItvalue = 80.0;
+
+enum Gender { male, female }
 
 class InputPage extends StatefulWidget {
   const InputPage({super.key});
@@ -13,14 +19,32 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-  static const activeColor = Color(0XFF1D1E33);
-  static const containerColor = Color(0XFFEB1555);
-  static const hItvalue = 80.0;
+  Color malecardcolor = inactiveColor;
+  Color femalecardcolor = inactiveColor;
+  void updateColor(Gender gender) {
+    if (gender == Gender.male) {
+      if (malecardcolor == inactiveColor) {
+        malecardcolor = activeColor;
+        femalecardcolor = inactiveColor;
+      } else {
+        malecardcolor = inactiveColor;
+      }
+    }
+    if (gender == Gender.female) {
+      if (femalecardcolor == inactiveColor) {
+        femalecardcolor = activeColor;
+        malecardcolor = inactiveColor;
+      } else {
+        femalecardcolor = inactiveColor;
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xFF0A0E1),
         title: Center(
           child: Text(
             "BMI CALCULATOR",
@@ -33,15 +57,29 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ReuseableCard(
-                    colour: activeColor,
-                    childIcon: SexWidget(FontAwesomeIcons.mars, "MALE"),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColor(Gender.male);
+                      });
+                    },
+                    child: ReuseableCard(
+                      colour: malecardcolor,
+                      childIcon: SexWidget(FontAwesomeIcons.mars, "MALE"),
+                    ),
                   ),
                 ),
                 Expanded(
-                  child: ReuseableCard(
-                    colour: activeColor,
-                    childIcon: SexWidget(FontAwesomeIcons.venus, "FEMALE"),
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColor(Gender.female);
+                      });
+                    },
+                    child: ReuseableCard(
+                      colour: femalecardcolor,
+                      childIcon: SexWidget(FontAwesomeIcons.venus, "FEMALE"),
+                    ),
                   ),
                 ),
               ],
