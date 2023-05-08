14208a1eda57e973253bf3a905ae63dd31c301c3
Dave RaGoose <daveyeinde@gmail.com>
2023-05-09 02:16:07 +0300

    all box edited

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -10,6 +10,10 @@ const activeColor = Color(0XFF1D1E63);
 const inactiveColor = Color(0XFF111328);
 const containerColor = Color(0XFFEB1555);
 const hItvalue = 80.0;
+int height = 179;
+int weight = 50;
+int age = 18;
+var result = "CALCULATE YOUR BMI";
 
 enum Gender { male, female }
 
@@ -87,7 +91,7 @@ class _InputPageState extends State<InputPage> {
                       textBaseline: TextBaseline.alphabetic,
                       children: [
                         Text(
-                          '189',
+                          height.toString(),
                           style: TextStyle(
                             fontSize: 40,
                             fontWeight: FontWeight.w900,
@@ -99,6 +103,29 @@ class _InputPageState extends State<InputPage> {
                         ),
                       ],
                     ),
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                          thumbColor: Color(0XFFEB1555),
+                          thumbShape:
+                              RoundSliderThumbShape(enabledThumbRadius: 15),
+                          overlayColor: Color(0X29FF155E),
+                          overlayShape:
+                              RoundSliderOverlayShape(overlayRadius: 30),
+                          activeTrackColor: Colors.white,
+                          inactiveTrackColor: Colors.grey),
+                      child: Slider(
+                        min: 120,
+                        max: 222,
+                        value: height.toDouble(),
+                        onChanged: (value) {
+                          setState(
+                            () {
+                              height = value.round();
+                            },
+                          );
+                        },
+                      ),
+                    )
                   ],
                 )),
           ),
@@ -108,21 +135,136 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: ReuseableCard(
                     colour: activeColor,
+                    childWij: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "WEIGHT",
+                          style: kTxtStyl,
+                        ),
+                        Text(
+                          weight.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.w900, fontSize: 60),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  weight--;
+                                });
+                              },
+                              child: Container(
+                                  margin: EdgeInsets.all(12),
+                                  height: 40,
+                                  width: 40,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey,
+                                      borderRadius: BorderRadius.circular(20)),
+                                  child: Icon(FontAwesomeIcons.minus)),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  weight++;
+                                });
+                              },
+                              child: Container(
+                                  margin: EdgeInsets.all(8),
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                      color: Color(0XFFEB1555),
+                                      borderRadius: BorderRadius.circular(20)),
+                                  child: Icon(FontAwesomeIcons.plus)),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
                   child: ReuseableCard(
                     colour: activeColor,
+                    childWij: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "AGE",
+                          style: kTxtStyl,
+                        ),
+                        Text(
+                          age.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.w900, fontSize: 60),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  age--;
+                                });
+                              },
+                              child: Container(
+                                  margin: EdgeInsets.all(12),
+                                  height: 40,
+                                  width: 40,
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey,
+                                      borderRadius: BorderRadius.circular(20)),
+                                  child: Icon(FontAwesomeIcons.minus)),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  age++;
+                                });
+                              },
+                              child: Container(
+                                  margin: EdgeInsets.all(8),
+                                  width: 40,
+                                  height: 40,
+                                  decoration: BoxDecoration(
+                                      color: Color(0XFFEB1555),
+                                      borderRadius: BorderRadius.circular(20)),
+                                  child: Icon(FontAwesomeIcons.plus)),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
           ),
           Container(
-            color: containerColor,
-            height: hItvalue,
-            width: double.infinity,
-          )
+              color: containerColor,
+              height: hItvalue,
+              width: double.infinity,
+              child: GestureDetector(
+                onTap: () {
+                  setState(() {
+                    int res = height + age + weight;
+                    result = res.toString();
+                  });
+                },
+                child: Center(
+                  child: Text(result,
+                      style: kTxtStyl.copyWith(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      )),
+                ),
+              ))
         ],
       ),
     );
